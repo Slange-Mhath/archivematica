499b48d3954966d00d8e4596d9b797683c8064f8
WebSepp <bastilange@t-online.de>
2021-03-11 13:59:01 +0000

    Add Ross new files without the changes on the old files
    This commit just adds the new files from Ross PR.1638.
    While v0 seems to be a splitted up version of
    the old verbose create_mets, the v1 seems to have adjustments
    regarding reduced METS file generation.
    This branch is just to investigate those approaches.

---
+++ src/MCPClient/lib/clientScripts/v1_create_mets/create_mets_dataverse_v2.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+"""Create METS Dataverse
+Maps Dataverse specific elements into the AIP METS file generated on
+ingest.
+"""
+
+from __future__ import print_function
+import sys
+
+import archivematicaFunctions
+from custom_handlers import get_script_logger
+
+import metsrw
+
+
+# Create a module level logger.
+logger = get_script_logger("archivematica.mcp.client.createMETSDataverse")
+
+
+def create_dataverse_sip_dmdsec(job, sip_path):
+    """
+    Return SIP-level Dataverse dmdSecs for inclusion in the AIP METS.
+    :param str sip_path: ...
+    :return: List of dmdSec Elements
+    """
+    logger.info("Create dataverse sip dmdsec %s", sip_path)
+    # Retrieve METS.xml from the file system.
+    metadata_mets_paths = archivematicaFunctions.find_metadata_files(
+        sip_path, "METS.xml", only_transfers=True
+    )
+    if not metadata_mets_paths:
+        return []
+    ret = []
+    for metadata_path in metadata_mets_paths:
+        try:
+            mets = metsrw.METSDocument.fromfile(metadata_path)
+        except mets.MetsError:
+            job.pyprint(
+                "Could not parse external METS (Dataverse)",
+                metadata_path,
+                file=sys.stderr,
+            )
+            continue
+        # Retrieve all directory DMDSecs from the METS.xml.
+        for f in mets.all_files():
+            if f.type == "Directory" and f.dmdsecs:
+                # Serialize
+                ret += [d.serialize() for d in f.dmdsecs]
+    return ret
+
+
+def create_dataverse_tabfile_dmdsec(job, sip_path, tabfile):
+    """
+    Returns dmdSec associated with the given tabfile, if one exists.
+    """
+    logger.info("Create Dataverse tabfile dmdsec %s", sip_path)
+    # Retrieve METS.xml from the file system.
+    metadata_mets_paths = archivematicaFunctions.find_metadata_files(
+        sip_path, "METS.xml", only_transfers=True
+    )
+    if not metadata_mets_paths:
+        return []
+    ret = []
+    for metadata_path in metadata_mets_paths:
+        try:
+            mets = metsrw.METSDocument.fromfile(metadata_path)
+        except mets.MetsError:
+            job.pyprint(
+                "Could not parse external METS (Dataverse)",
+                metadata_path,
+                file=sys.stderr,
+            )
+            continue
+        # Retrieve all Item DMDSecs from the METS.xml.
+        for f in mets.all_files():
+            if f.type == "Item" and f.path.endswith(tabfile):
+                # Found the correct tabfile
+                return [d.serialize() for d in f.dmdsecs]
+    return ret
---
+++ src/MCPClient/lib/clientScripts/v1_create_mets/create_mets_metadata_csv.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+
+import collections
+import csv
+import sys
+import traceback
+
+# archivematicaCommon
+import archivematicaFunctions
+
+
+def parseMetadata(job, SIPPath, state):
+    """
+    Parse all metadata.csv files in SIPPath.
+    Looking for metadata.csvs in metadata/ and
+    objects/metadata/transfers/<transfer name>/metadata/
+    See parseMetadataCSV for details on parsing.
+    :param SIPPath: Path to the SIP
+    :return: {<filename>: OrderedDict(key: [values]) }
+    """
+    all_metadata = {}
+    metadata_csvs = archivematicaFunctions.find_metadata_files(SIPPath, "metadata.csv")
+
+    for metadataCSVFilePath in metadata_csvs:
+        try:
+            csv_metadata = parseMetadataCSV(job, metadataCSVFilePath)
+        except Exception:
+            job.pyprint("error parsing: ", metadataCSVFilePath, file=sys.stderr)
+            job.print_error(traceback.format_exc())
+            state.error_accumulator.error_count += 1
+            continue
+        # Provide warning if this file already has differing metadata
+        # Not using all_metadata.update(csv_metadata) because of that
+        for entry, values in csv_metadata.items():
+            if entry in all_metadata and all_metadata[entry] != values:
+                job.pyprint(
+                    "Metadata for",
+                    entry,
+                    "being updated. Old:",
+                    all_metadata[entry],
+                    "New:",
+                    values,
+                    file=sys.stderr,
+                )
+            existing = all_metadata.get(entry, collections.OrderedDict())
+            existing.update(values)
+            all_metadata[entry] = existing
+
+    return all_metadata
+
+
+def parseMetadataCSV(job, metadataCSVFilePath):
+    """
+    Parses the metadata.csv into a dict with entries for each file.
+    Each file's entry is an OrderedDict containing the column header and a list of values for each column.
+    Example CSV:
+    Filename,dc.title,dc.type,dc.type,Other metadata
+    objects/foo.jpg,Foo,Photograph,Still Image,Taken on a sunny day
+    objects/bar/,Bar,Photograph,Still Image,All taken on a rainy day
+    Produces:
+    {
+        'objects/foo.jpg': OrderedDict(dc.title=[Foo], dc.type=[Photograph, Still Image], Other metadata=[Taken on a sunny day])
+        'objects/bar': OrderedDict(dc.title=[Bar], dc.date=[Photograph, Still Image], Other metadata=[All taken on a rainy day])
+    }
+    :param metadataCSVFilePath: Path to the metadata CSV to parse
+    :return: {<filename>: OrderedDict(<metadata name>: [<metadata value>]) }
+    """
+    metadata = {}
+    # use universal newline mode to support unusual newlines, like \r
+    with open(metadataCSVFilePath, "rbU") as f:
+        reader = csv.reader(f)
+        # Parse first row as header
+        header = next(reader)
+        # Strip filename column, strip whitespace from header values
+        header = [h.strip() for h in header[1:]]
+        # Parse data
+        for row in reader:
+            if not row:
+                continue
+            entry_name = row[0]
+            if entry_name.endswith("/"):
+                entry_name = entry_name[:-1]
+            # Strip file/dir name from values
+            row = row[1:]
+            values = archivematicaFunctions.OrderedListsDict(zip(header, row))
+            if entry_name in metadata and metadata[entry_name] != values:
+                job.pyprint(
+                    "Metadata for",
+                    entry_name,
+                    "being overwritten. Old:",
+                    metadata[entry_name],
+                    "New:",
+                    values,
+                    file=sys.stderr,
+                )
+            metadata[entry_name] = values
+
+    return collections.OrderedDict(metadata)  # Return a normal OrderedDict
